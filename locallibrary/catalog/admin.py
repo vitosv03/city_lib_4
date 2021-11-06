cf6efd52996d4b26d33cd01ac2848e30d17a5357
vitosv03 <vitosv03@gmail.com>
2021-11-06 13:32:15 +0200

    Регистрация моделей

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Book, BookInstance, Language
 
 admin.site.register(Book)
 admin.site.register(Author)
