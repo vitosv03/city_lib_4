46ef79d40d893a317d76130d218326394e807ed7
vitosv03 <vitosv03@gmail.com>
2021-11-06 21:56:43 +0200

    5-Отображения (на основе функций)

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -2,3 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Book, Author, BookInstance, Genre, Language
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books,
+                 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available,
+                 'num_authors': num_authors
+                 },
+    )
