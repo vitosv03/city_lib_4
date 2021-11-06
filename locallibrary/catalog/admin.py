503bbdbe9dbabe9e176e2d516f379d135b0fa9c9
vitosv03 <vitosv03@gmail.com>
2021-11-06 15:23:06 +0200

    Управление отображаемыми и вложенными полями

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -12,11 +12,11 @@ admin.site.register(Language)
 
 
 # Define the admin class
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 # Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
 
 
 # Register the Admin classes for Book using the decorator
