f99071e8300d7fb1dff326be7f13f2001fc61027
vitosv03 <vitosv03@gmail.com>
2021-11-06 23:16:26 +0200

    5-получать из базы данных количество жанров перемеиновал переменную

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -17,7 +17,7 @@ def index(request):
     num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
 
     # выполнение задания 2 из блока 5
-    num_gernes = Genre.objects.all().count()
+    num_genres = Genre.objects.all().count()
 
 
     # Отрисовка HTML-шаблона index.html с данными внутри
@@ -29,6 +29,6 @@ def index(request):
                  'num_instances': num_instances,
                  'num_instances_available': num_instances_available,
                  'num_authors': num_authors,
-                 'num_gernes': num_gernes
+                 'num_genres': num_genres
                  },
     )
