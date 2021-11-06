8e154a308086dec566472c41375040ae63479358
vitosv03 <vitosv03@gmail.com>
2021-11-06 15:29:02 +0200

    Управление отображаемыми и вложенными полями

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -16,6 +16,7 @@ admin.site.register(Language)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    # exclude = ['date_of_death']
 # Register the admin class with the associated model
 
 
