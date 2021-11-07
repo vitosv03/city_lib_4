19a9607b03e0efd6f73741399235fec9ab568aea
vitosv03 <vitosv03@gmail.com>
2021-11-07 10:05:24 +0200

    6-Преобразования URL-адресов

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -9,3 +9,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+]
