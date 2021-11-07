3b306f12b2d7011f4d443507c63882f5491278a4
vitosv03 <vitosv03@gmail.com>
2021-11-07 11:31:56 +0200

    6-Создание шаблона детальной информации

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -105,25 +105,20 @@ class BookInstance(models.Model):
         return '%s (%s)' % (self.id,self.book.title)
 
 class Author(models.Model):
-    """
-    Model representing an author.
-    """
-    # author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    """    Model representing an author.    """
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
 
     def get_absolute_url(self):
-        """
-        Returns the url to access a particular author instance.
-        """
+        """        Returns the url to access a particular author instance.        """
         return reverse('author-detail', args=[str(self.id)])
 
 
     def __str__(self):
-        """
-        String for representing the Model object.
-        """
+        """        String for representing the Model object.        """
         return '%s, %s' % (self.last_name, self.first_name)
 
+    class Meta:
+        ordering = ['last_name']
