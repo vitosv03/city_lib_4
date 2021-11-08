e6af9f62b65e5a6ee4f39a5ee21e4427babc6765
vitosv03 <vitosv03@gmail.com>
2021-11-08 22:32:34 +0200

    7-Добавить список на боковую панель

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
 from django.contrib.auth.models import User
 from datetime import date
-
+import uuid  # Required for unique book instances
 
 # Create your models here.
-#
 
 class Genre(models.Model):
     """
@@ -70,15 +69,9 @@ class Book(models.Model):
     display_language.short_description = 'Language'
 
 
-import uuid  # Required for unique book instances
-
-
 class BookInstance(models.Model):
-    """
-    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
-                          help_text="Unique ID for this particular book across whole library")
+    """    Model representing a specific copy of a book (i.e. that can be borrowed from the library).    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
@@ -89,15 +82,12 @@ class BookInstance(models.Model):
         ('a', 'Available'),
         ('r', 'Reserved'),
     )
-    status = models.CharField(max_length=1,
-                              choices=LOAN_STATUS,
-                              blank=True,
-                              default='m',
-                              help_text='Book availability'
-                              )
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
 
     class Meta:
         ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
 
     @property
     def is_overdue(self):
@@ -106,9 +96,7 @@ class BookInstance(models.Model):
         return False
 
     def __str__(self):
-        """
-        String for representing the Model object
-        """
+        """        String for representing the Model object        """
         return '%s (%s)' % (self.id, self.book.title)
 
 
