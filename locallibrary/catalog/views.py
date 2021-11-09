f9d120dd5395da331119851604bbccb5269c8506
vitosv03 <vitosv03@gmail.com>
2021-11-09 20:55:20 +0200

    9-отображение должно знать вызвано ли оно в первый раз для отрисовки формы по умолчанию, а если это не так, то провести валидацию полученных данных.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -86,3 +86,40 @@ class LoanedBooksByAllListView(LoginRequiredMixin, generic.ListView):
             filter(status__exact='o'). \
             order_by('due_back')
 
+
+from django.contrib.auth.decorators import permission_required
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import datetime
+
+from .forms import RenewBookForm
+
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    """    Функция просмотра для обновления определенного экземпляра BookInstance библиотекарем  """
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+
+    # Если это запрос POST, обработайте данные формы
+    if request.method == 'POST':
+
+        # Создайте экземпляр формы и заполните его данными из запроса (привязки):
+        form = RenewBookForm(request.POST)
+
+        # Проверьте, действительна ли форма:
+        if form.is_valid():
+            # обрабатываем данные в form.cleaned_data по мере необходимости
+            # (здесь мы просто записываем их в поле due_back модели)
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+
+            # перенаправить на новый URL:
+            return HttpResponseRedirect(reverse('all-borrowed') )
+
+    # Если это GET (или любой другой метод), создайте форму по умолчанию.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+
+    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
