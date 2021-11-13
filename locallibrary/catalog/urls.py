53f9302fb55499b708f8d0dcf92679eafdf0f88e
vitosv03 <vitosv03@gmail.com>
2021-11-13 08:08:46 +0200

    9- добавил два шаблона HTML редактирвоания автора и удаление автора

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -28,3 +28,9 @@ urlpatterns += [
 urlpatterns += [
     url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
 ]
+
+urlpatterns += [
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+]
