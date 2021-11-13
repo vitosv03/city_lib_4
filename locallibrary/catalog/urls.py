4e0717d275c22296df7c89850480f7e889abbce7
vitosv03 <vitosv03@gmail.com>
2021-11-13 08:25:59 +0200

    9-Задание. прописал ссылки для страниц по созданию, редактированию и удалению книг

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -34,3 +34,9 @@ urlpatterns += [
     url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
     url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
 ]
+
+urlpatterns += [
+    url(r'^book/create/$', views.BookCreate.as_view(), name='book_create'),
+    url(r'^book/(?P<pk>\d+)/update/$', views.BookUpdate.as_view(), name='book_update'),
+    url(r'^book/(?P<pk>\d+)/delete/$', views.BookDelete.as_view(), name='book_delete'),
+]
