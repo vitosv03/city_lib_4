081dc451007a247bd5286befa8bf903dfbef4170
vitosv03 <vitosv03@gmail.com>
2021-11-14 15:11:28 +0200

    10-добавил тест. для добавления нового автора

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -20,10 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^k0gq@#@+(ohowfk*l%^-y%!&9yc4aq1jci@83(%2@0*z2s58c'
+# SECRET_KEY = 'django-insecure-^k0gq@#@+(ohowfk*l%^-y%!&9yc4aq1jci@83(%2@0*z2s58c'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
